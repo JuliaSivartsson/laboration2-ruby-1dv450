62866c12e1fff273facd69fbd72567ab9e4538c6
JuliaSivartsson <jsigc09@student.lnu.se>
2016-02-02 10:20:49 +0000

    Changed how apiKeys looks

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -8,10 +8,11 @@ class AppsController < ApplicationController
     
     def create
         #Create a new application
-        random = rand(2000...5000)
+        apiKey = generate_api_key
+        
         @application = App.new(apps_params)
         if @application.save
-            @application.update_attribute(:apikey, random)
+            @application.update_attribute(:apikey, apiKey)
           flash[:notice] = "Yay, your registration was successful"
           flash[:color] = "valid"
         else
@@ -30,5 +31,13 @@ class AppsController < ApplicationController
     
     def apps_params
         params.require(:application).permit(:name, :description, :user_id, :apikey)
+    end
+    
+    #Found help on how to generate apiKey here: http://blog.joshsoftware.com/2014/05/08/implementing-rails-apis-like-a-professional/
+    def generate_api_key
+      loop do
+        token = SecureRandom.base64.tr('+/=', 'Qrt')
+        break token
+      end
     end
 end
