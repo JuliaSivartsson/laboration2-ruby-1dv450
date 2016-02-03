032d89b83172ee33cd34fd91b1372c455891b990
JuliaSivartsson <jsigc09@student.lnu.se>
2016-02-03 08:26:09 +0000

    added a helper method for changing titles in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
     
-        def is_admin
+    #Checks if logged in user is admin
+    def is_admin
         return true if @current_user.name == "admin" #The admin account
+    end
+    
+    #Help setting title, found help here http://stackoverflow.com/questions/3059704/rails-3-ideal-way-to-set-title-of-pages
+    def title(page_title)
+        content_for :title, page_title.to_s
     end
 end
