8e97abc7c454245c26bf70bc63951b40875cab32
juliasivartsson <jsigc09@student.lnu.se>
2016-03-11 15:46:08 +0000

    trying to get delete to work

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -12,9 +12,10 @@ class Api::V1::ApiController < ApplicationController
     # For all responses in this controller, return the CORS access control headers.
     
     def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Origin'] = 'https://laboration3-angular-juliasivartsson.c9users.io'
         headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
         headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        headers['Allow'] = 'OPTIONS'
     end
 end
