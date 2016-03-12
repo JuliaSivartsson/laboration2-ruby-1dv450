408c282ee5a0df97332aa56c0209146bfff352d8
juliasivartsson <jsigc09@student.lnu.se>
2016-03-12 10:16:00 +0000

    cors not working?

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -7,14 +7,15 @@ class Api::V1::ApiController < ApplicationController
     #Return response in json or xml
     respond_to :json, :xml
     
-  after_filter :cors_set_access_control_headers
+  #after_filter :cors_set_access_control_headers
 
     # For all responses in this controller, return the CORS access control headers.
     
-    def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-        headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    end
+   # def cors_set_access_control_headers
+#        headers['Access-Control-Allow-Origin'] = '*'
+#        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE, PUT'
+#        headers['Access-Control-Request-Method'] = '*'
+#        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+#        headers['Access-Control-Max-Age'] = "5000";
+#    end
 end
