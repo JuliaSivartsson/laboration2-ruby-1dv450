4d1fce4edb5b0f09e14c766541b2a42de6364bb0
juliasivartsson <jsigc09@student.lnu.se>
2016-03-12 10:03:29 +0000

    test

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::ApiController < ApplicationController
     
     def cors_set_access_control_headers
         headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, DELETE, GET, PUT'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
         headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
