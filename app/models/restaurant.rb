c7f51b49b588de2b7fc256875ff79a1ab120a684
juliasivartsson <jsigc09@student.lnu.se>
2016-03-15 14:52:03 +0000

    rating has to be a number

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
     validates :name, presence: true, length: { in: 3..100 }
     validates :message, presence: true
-    validates :rating, presence: true, :inclusion => 0..5
+    validates :rating, presence: true, :in => 0..5
     validates :position, presence: true
     
     belongs_to :position
